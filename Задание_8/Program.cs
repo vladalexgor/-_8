e29a2a0ca96e2d6446d0958f69a796368f07e1ea
vladalexgor@ya.ru <vladalexgor@ya.ru>
2021-11-28 10:35:36 +0300

    Добавьте файлы проекта.

---
+++ Задание_8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_8
+{
+    class Program
+    {
+        //Выберите любую папку на своем компьютере, имеющую вложенные директории. Выведите на консоль ее содержимое и содержимое всех подкаталогов.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите директорию любой папки на своем компе, имеющую вложенные директории:");
+            string path = Console.ReadLine();
+            string[] X = Directory.GetDirectories(path);
+            foreach (string D in X)
+            {
+                Console.WriteLine("Каталог: {0}", D);
+                string[] Y = Directory.GetDirectories(D);
+                foreach (string P in Y)
+                {
+                    Console.WriteLine("Подкаталог: - {0}", P);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание_8_App2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_8_App2
+{
+    class Program
+    {
+        //Программно создайте текстовый файл и запишите в него 10 случайных чисел. Затем программно откройте созданный файл, рассчитайте сумму чисел в нем, ответ выведите на консоль.
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\gorshkov\Desktop\ИТМО\Задание_8\Test.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+
+            }    
+            /*{
+                Random random = new Random();
+                array [i] = random.Next();
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.Write("Старт программы");
+                }
+                sw.WriteLine("Старт программы");
+                sw.WriteLine("Ошибка");
+            }
+
+
+
+
+            /*string path = "Logs2/Log.txt";
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine("Старт программы");
+                sw.WriteLine("Ошибка");
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            Console.ReadKey();
+
+            /*if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            
+            
+            /*DirectoryInfo directory = new DirectoryInfo(path);
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+
+            /*string path = "Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }*/
+
+            /*DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo d in drives)
+            {
+                Console.WriteLine("Название: {0}", d.Name);
+                if (d.IsReady)
+                {
+                    Console.WriteLine("Объем диска: {0}", d.TotalSize);
+                    Console.WriteLine("Свободно: {0}", d.AvailableFreeSpace);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();*/
+        }
+    }
+}
